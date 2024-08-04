0d413fff0bd7a2bc3ed482d2c4e46bec692633a9
Visualdevelopment <permachmuti@gmail.com>
2024-08-04 02:32:24 +0200

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+-- fxmanifest.lua
+
+fx_version 'adamant'
+game 'gta5'
+
+author 'Visuals_development'
+description 'Farming script with random key press events for farming cocaine'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'es_extended',
+    'ox_lib'
+}
